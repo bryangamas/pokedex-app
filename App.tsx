2792e34ab99e7f673877b4b9fd9a278e561c6e80
Bryan Gama <bryangaso15@gmail.com>
2021-11-13 23:23:13 -0500

    feat: using typescript + icons

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import NavigationDrawer from "./src/navigation/NavigationDrawer";
 
@@ -10,12 +9,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>We are on HomeScreen</Text>
+      <Button title="Settings" />
+    </View>
+  );
+}
